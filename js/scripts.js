e4144ff54a604a9fcef672c2e438f33d29b3c03e
Iader Folli <iader_93@hotmail.it>
2022-12-21 19:42:31 +0100

    agg

--- js/scripts.js
+++ js/scripts.js
@@ -3,41 +3,82 @@
 console.log("prima del ciclo");
 
 for (let i = 1; i <= 100; i++) {
+
     // CREO UN DIV X OGNI (BOX-NUM)
+
+    console.log("durante il ciclo", i);
+
+    const div = document.createElement('div');
+
+    console.log("questo è il div creato:", div)
 
     // GLI ASSEGNO UNA CLASSE GENERICA PER LO STILE DI BASE
 
-    console.log("durante il ciclo", i);
+    div.className = 'general-box';
+
+    console.log("questo è il nome (classe) del div creato:", div.className)
 
     if (i % 3 == 0 && i % 5 == 0) {
 
         // GLI ASSEGNO LA CLASSE CON IL BG SPECIFICO (DA SPECIFICARE IN CSS O BOOTSTRAP)
 
+        div.className = 'fizzbuzz-box';
+
+        div.textContent = "FizzBuzz";
+
+        console.log("questo è il nome (classe) del div creato:", div.className)
+
         console.log("questo è un multiplo di 3 e 5")
 
-        document.getElementById('mio-div').innerHTML += "FizzBuzz ";
+        // document.getElementById('fizzbuzz-box').innerHTML += "FizzBuzz ";
 
-        console.log(" questo deve essere FizzBuzz")
+        console.log("questo deve essere FizzBuzz")
 
     } else if (i % 3 == 0) {
 
         // GLI ASSEGNO LA CLASSE CON IL BG SPECIFICO (DA SPECIFICARE IN CSS O BOOTSTRAP)
+
+        div.className = 'fizz-box';
+
+        div.textContent = "Fizz";
+
+        console.log("questo è il nome (classe) del div creato:", div.className)
+
         console.log("questo è un multiplo di 3")
 
-        document.getElementById('mio-div').innerHTML += "Fizz ";
+        // document.getElementById('fizz-box').innerHTML += "Fizz ";
 
-        console.log(" questo deve essere fizz")
+        console.log("questo deve essere fizz")
+
     } else if (i % 5 == 0) {
+
         // GLI ASSEGNO LA CLASSE CON IL BG SPECIFICO (DA SPECIFICARE IN CSS O BOOTSTRAP)
+
+        div.className = 'buzz-box';
+
+        div.textContent = "Buzz";
+
+        console.log("questo è il nome (classe) del div creato:", div.className)
+
         console.log("questo è un multiplo di 5")
 
-        document.getElementById('mio-div').innerHTML += "Buzz ";
+        // document.getElementById('buzz-box').innerHTML += "Buzz ";
 
-        console.log(" questo deve essere buzz")
+        console.log("questo deve essere buzz")
+
     } else {
+
         document.getElementById('mio-div').innerHTML += i + " ";
+
     }
+
     // FACCIO L'APPEND DEL DIV CREATO NEL "mio-div" (cio' significa che trascrivo questo div nell'HTML)
+
+    const mioDiv = document.querySelector('#mio-div');
+
+    console.log("questo è l'Append creato:", mioDiv)
+
+    mioDiv.appendChild(div);
 }
 
 console.log("dopo il ciclo");
